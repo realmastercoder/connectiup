a7ae65af70d168b7c8704c20717d9b2433c08444
AlecMParfitt <ppfr@iup.edu>
2023-03-01 00:28:24 -0500

    login cleanup

--- login.php
+++ login.php
@@ -26,6 +26,12 @@
             session_start();
             // start a session and redirect to homepage
             $_SESSION['username'] = $username;
+            // query the db for the user's id
+            $query = "SELECT id FROM users WHERE username='$username'";
+            $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+            $row = mysqli_fetch_assoc($result);
+            $id = $row['id'];
+            $_SESSION['id'] = $id;
             header('Location: homepage.php');
         }else{
             $fmsg = "Invalid Login Credentials.";
