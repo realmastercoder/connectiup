f3e4d0e75baaf3f9c7e49a02eb182d8d3d625e77
jordanweir12 <jordanweir247@gmail.com>
2023-03-27 14:48:36 -0400

    sidebar

--- homepage.php
+++ homepage.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -21,6 +20,7 @@ if (!$conn) {
     <title>ConnectIUP</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/sidebar.css">
 </head>
 
 <body>
@@ -41,8 +41,56 @@ if (!$conn) {
             <button>Search</button>
         </form>
     </nav>
+
+    <div class="sidebar">
+            <hr/>
+            <h3>Rules for Posting</h3>
+            <p>When writing posts and replies, users must keep in mind
+                the following rules&ast;:</p>
+            <ol>
+                <li>Users shall be respectful and courteous in their posts 
+                    and replies to other users.</li><br/>
+
+                <li>Users shall not share any explicit content in the 
+                    form of pictures or words. Content falling under this 
+                    category includes pornographic or sexual material, explicit 
+                    language, etc. </li><br/>
+
+                <li>Users shall refrain from posting content with harmful intent. 
+                    This includes no demeaning, discriminatory, or harassing 
+                    behavior in posts and replies. </li><br/>
+
+                <li>Users shall not spam post or reply. </li><br/>
+
+                <li>Users shall not post copyright infringing material. </li><br/>
+
+                <li>Users shall not engage in any illegal activity 
+                    in their posts and replies. </li>    
+            </ol>
+
+            <p>&ast;Refusal to comply with the rules listed above will result 
+                in consequences for the user. These consequences are outlined 
+                in ConnectIUP&apos;s Terms and Conditions that all users agree to in 
+                order to use ConnectIUP. To review our site&apos;s Terms and Conditions, 
+                click <a href="terms_cond.html">here</a>. 
+            </p>
+            <hr/>
+            
+            <h3>Helpful Links</h3>
+            <ul>
+                <li><a href="about.php">About ConnectIUP</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+                <li><a href="terms_cond.html">Terms and Conditions</a></li>
+                <li><a href="#">FAQ</a></li> <!--FAQ page does not exist-->
+                <li><a href="#">Rules for Posting</a></li> <!--Post rules page does not exist-->
+            </ul>
+            <hr/>
+    </div> 
+
+    <div class="maincontent">
     <h2>Welcome, <?php echo $_SESSION['username'];?>!</h2>
     <hr>
+    
     <?php
             $sql = "SELECT * from posts ORDER BY id DESC";
             $result = mysqli_query($conn, $sql);
@@ -70,6 +118,7 @@ if (!$conn) {
     <div id="post">
         
     </div>
+        </div>
     
 </body>
 
